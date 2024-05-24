c1159714fa8d63fab58838fc871a27c1fa035d1b
David Drobnak <david.drobnak26@gmail.com>
2024-05-24 23:15:04 +0200

    added gitsigns plugin

--- lua/david/packer.lua
+++ lua/david/packer.lua
@@ -21,6 +21,7 @@ return require('packer').startup(function(use)
   }
   use('tpope/vim-commentary')
   use('tpope/vim-surround')
+  use('lewis6991/gitsigns.nvim')
 
   -- Color scheme & LSP
   use({
